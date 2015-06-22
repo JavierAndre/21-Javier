6e97bbe5b12384cf4fb98af6074ffb0f29fc2c1f
JavierKazag <javier@javierandre.com>
2015-06-22 14:34:34 -0700

    Test change.

--- 21-Javier/src/twentyOne/App.java
+++ 21-Javier/src/twentyOne/App.java
@@ -5,11 +5,9 @@ public class App
 	public static void main(String[] args)
 	{
 		// Create an instance of the Controller class
-		
 		Controller controller = new Controller();
 		
 		// Create an instance of the View class
-		
 		View view = new View(controller);
 	}
 }
